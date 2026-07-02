622ff64ffc05ab6efed1a6c74613b35e40fcfd10
mohammadgeraylo2-alt <mohammadgeraylo2@gmail.com>
2026-07-02 19:42:42 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ if [ ! -f /freqtrade/user_data/config.json ]; then
     cp -r /freqtrade/user_data_seed/. /freqtrade/user_data/
 fi
 
-exec freqtrade trade --config /freqtrade/user_data/config.json --strategy SampleStrategy
+# The volume is owned by root; freqtrade must run as ftuser.
+chown -R ftuser:ftuser /freqtrade/user_data
+
+exec su ftuser -c "freqtrade trade --config /freqtrade/user_data/config.json --strategy SampleStrategy"
