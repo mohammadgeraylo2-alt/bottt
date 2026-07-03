e80a66f2dc5b1c71ff3984b85d5f9e1b2247f19b
mohammadgeraylo2-alt <mohammadgeraylo2@gmail.com>
2026-07-03 17:45:57 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,12 @@ if [ ! -f /freqtrade/user_data/config.json ]; then
     cp -r /freqtrade/user_data_seed/. /freqtrade/user_data/
 fi
 
+# Always keep the strategy file in sync with the latest build (safe to overwrite,
+# it's not user data).
+mkdir -p /freqtrade/user_data/strategies
+cp /freqtrade/user_data_seed/strategies/NostalgiaForInfinityX7.py /freqtrade/user_data/strategies/
+
 # The volume is owned by root; freqtrade must run as ftuser.
 chown -R ftuser:ftuser /freqtrade/user_data
 
-exec su ftuser -c "freqtrade trade --config /freqtrade/user_data/config.json --strategy SampleStrategy"
+exec su ftuser -c "freqtrade trade --config /freqtrade/user_data/config.json --strategy NostalgiaForInfinityX7"
